22c039fc92edb7ff73fa03939068728c8ffad4c2
Chetan Muneshwar <chetan.muneshwar@betterlabs.net>
2012-07-19 01:39:38 +0530

    updates for schdueling archive jobs

--- lib/rsync.rb
+++ lib/rsync.rb
@@ -7,14 +7,13 @@
   require 'mail'  
   Dir["./mailer.rb"].each {|file| require file }
   @@log = Logger.new( 'log/rsync.log', 'daily' )
-  @@t = Time.new
-  @@date_time = "#{@@t.year}-#{@@t.month}-#{@@t.day}"
+  @@date_time = "#{Time.now.strftime("%Y-%m-%d-%H%M%S")}"
   class Rsync
     attr_accessor :server_name, :ipaddress, :password, :username, :port, :folder_array , :backup_path , :exclude_array, :schedule
 
-  def initialize(config_name)
-    @config_name = config_name
-  end
+    def initialize(config_name)
+      @config_name = config_name
+    end
   # reads given argument as yml
   def read_config
     config = YAML.load_file("#{@config_name}")
@@ -31,24 +30,42 @@
   def take_backup
    @@log.debug "backup...started for #{@ipaddress} at [#{Time.new}]"
    @folder_array.split(",").each(&:lstrip!).each do |folder|
+     sub_folder = folder.split(":").each(&:lstrip!)
      @exclude_final= ""
      @exclude_array.split(",").each(&:lstrip!).each do |exclude|
        @exclude_final =  @exclude_final + ' --exclude \'' + exclude + '\''
      end   
-    cmd = "rsync -Ravzrq --skip-compress=*.tgz,*.gz,*.bz2,*.iso,*.jpg,*.jpeg,*.tif,*.tiff,*.tar,*.zip,*.mov,*.png,*.gif --delete #{@exclude_final} -e 'ssh -p#{@port}' #{@username}@#{@ipaddress}:#{folder} #{@backup_path}"
+     cmd = "rsync -Ravzrq --skip-compress=tgz/gz/bz2/7z/bz2/iso/jpg/jpeg/tif/tiff/tar/zip/mov/png/gif/mp[34] --delete #{@exclude_final} -e 'ssh -p#{@port}' #{@username}@#{@ipaddress}:#{sub_folder[0]} #{@backup_path}"
      # system execution command for rsync 
      %x{#{cmd}}  
+     p cmd
    end
    @@log.debug "backup...completed for #{@ipaddress} at [#{Time.new}]"
  end
  def compress_daily
   tar_cmd = "tar -zcvf #{@backup_path}.[#{@@date_time}].tar.gz #{@backup_path} && cd -"
-   @@log.debug "Started daily compression #{@ipaddress} at [#{Time.new}]"
+  @@log.debug "Started daily compression #{@ipaddress} at [#{Time.new}]"
   p tar_cmd
   %x{#{tar_cmd}} 
-   @@log.debug "Completed daily compression #{@ipaddress} at [#{Time.new}]"
+  @@log.debug "Completed daily compression #{@ipaddress} at [#{Time.new}]"
+end
+end
+class Archive < Rsync
+
+ def self.archive_on_schedule(data_path,server_name,archive_schedule)
+  s_tar_cmd = "tar -zcvf Archives/#{server_name}#{data_path.gsub(/\//, '-')}-#{archive_schedule}.#{Time.now.strftime("%Y-%m-%d-%H%M%S")}.tar.gz #{data_path} "
+  p s_tar_cmd
+  %x{#{s_tar_cmd}}
+  
 end
 
 end
+
+## TEST 
+#rsync_instance = Archive.new("./config/ishy.yml")
+#rsync_instance.read_config
+#rsync_instance.archive_on_schedule("/data/current_backups/ishy/backup/mybackupsql","test","hourly")
+
+#Archive.archive_on_schedule("/data/current_backups/ishy/backup/mybackupsql","test","hourly")
 
 
--- run.rb
+++ run.rb
@@ -10,57 +10,99 @@ Sidekiq.configure_client do |config|
 end
 
 
-class MyWorker 
+class RsyncWorker 
   include Sidekiq::Worker
-# timeout must be more for rsync operations
-    sidekiq_options :concurrency => 25
-    sidekiq_options :environment => nil
-    sidekiq_options :timeout => 64800
-    sidekiq_options :enable_rails_extensions => false
+  sidekiq_options :concurrency => 25
+  sidekiq_options :environment => nil
+  sidekiq_options :timeout => 64800
+  sidekiq_options :enable_rails_extensions => false
   def perform(config_yml)
    p "am here #{config_yml}"
    rsync_instance = Rsync.new(config_yml)
    rsync_instance.read_config
    rsync_instance.take_backup
-   #rsync_instance.compress_daily	
+   rsync_instance.compress_daily	
+ end
+end
+class ArchiveWorker
+  include Sidekiq::Worker
+  sidekiq_options :concurrency => 25
+  sidekiq_options :environment => nil
+  sidekiq_options :timeout => 64800
+  sidekiq_options :enable_rails_extensions => false
+  def perform(data_path,server_name,archive_schedule)
+  p "am here for compression #{data_path} #{server_name} #{archive_schedule}"
+  Archive.archive_on_schedule("#{data_path}","#{server_name}","#{archive_schedule}")
   end
 end
-class Yml_process < Rsync   
 
-    def generate_job(yml_file)
-         @schedule.split(",").each(&:lstrip!).each do |single_schedule|
-          case single_schedule
+class YmlProcess < Rsync   
 
-           when single_schedule = "hourly"
-	        doc = "\t every(1.hour, 'For \"#{yml_file}\"') do \n \t \t  MyWorker.perform_async(\"#{yml_file}\") \n  \t end \n" 
-		File.open('./clock.rb','a') {|f| f.write(doc) }
-           when single_schedule = "daily"
-	        doc = "\t every(1.day, 'For \"#{yml_file}\"') do \n \t \t MyWorker.perform_async(\"#{yml_file}\") \n  \t end \n" 
-	        File.open('./clock.rb','a') {|f| f.write(doc) }	
-           when single_schedule = "weekly"
-	        doc = "\t every(1.week, 'For \"#{yml_file}\"') do \n \t \t MyWorker.perform_async(\"#{yml_file}\") \n \t end \n" 
-                File.open('./clock.rb','a') {|f| f.write(doc) }
-          end
-         end
-     end
+  def generate_rsync_job(yml_file)
+   @schedule.split(",").each(&:lstrip!).each do |single_schedule|
+    case single_schedule
+
+    when single_schedule = "hourly"
+     doc = "\t every(1.hour, 'For #{yml_file}', :at => '**:30' ) do \n \t \t  RsyncWorker.perform_async(\"#{yml_file}\") \n  \t end \n" 
+     File.open('./clock.rb','a') {|f| f.write(doc) }
+    when single_schedule = "daily"
+     doc = "\t every(1.day, 'For #{yml_file}', :at => '05:00' ) do \n \t \t RsyncWorker.perform_async(\"#{yml_file}\") \n  \t end \n" 
+     File.open('./clock.rb','a') {|f| f.write(doc) }	
+    when single_schedule = "weekly"
+     doc = "\t every(1.week, 'For #{yml_file}', :at => '04:00') do \n \t \t RsyncWorker.perform_async(\"#{yml_file}\") \n \t end \n" 
+     File.open('./clock.rb','a') {|f| f.write(doc) }
+   end
+ end
 end
 
 
-   def yml_process 
-     yml_files = " "
-     yml_files << `ls  ./config/*.yml |xargs`
-      if yml_files.empty? 
-       puts "Err list empty"	
-      else
-	doc = "# ClockWork scheduler generated at [#{Time.new}] \n require \"clockwork\" \n \n Dir[\"./lib/rsync.rb\"].each {|file| require file } \n Dir[\"./run.rb\"].each {|file| require file } \n \n module Clockwork \n handler do |job| \n \t puts \"Running \#{job}\" \n end  \n" 
-	
-	File.open('./clock.rb','w+') {|f| f.write(doc) }
 
-        yml_files.split(" ").each do |yml_file|
-	  a = Yml_process.new(yml_file)
-	  a.read_config
-	  a.generate_job(yml_file)
-	end
-	File.open('./clock.rb','a') {|f| f.write("  end \n") }
-       end
+def generate_compress_schedule
+
+  @folder_array.split(",").each(&:lstrip!).each do |folder|
+#           if folder.split(":").length <= 1
+#	   	p "nothing found re"
+#	   else
+if folder.split(":").length > 1
+ sub_folder = folder.split(":").each(&:lstrip!)
+ sub_folder[1].split("|").each(&:lstrip!).each do |archive_schedule|
+  case archive_schedule
+
+  when archive_schedule = "hourly"
+    doc = "\t every(1.hour, 'For #{archive_schedule}', :at => '**:30' ) do \n \t \t  ArchiveWorker.perform_async(\"#{@backup_path.gsub(/\/$/, '')}#{sub_folder[0]}\",\"#{@server_name}\",\"#{archive_schedule}\") \n  \t end \n"
+    File.open('./clock.rb','a') {|f| f.write(doc) }
+  when archive_schedule = "daily"
+    doc = "\t every(1.day, 'For #{archive_schedule}', :at => '15:00' ) do \n \t \t ArchiveWorker.perform_async(\"#{@backup_path.gsub(/\/$/, '')}#{sub_folder[0]}\",\"#{@server_name}\",\"#{archive_schedule}\") \n  \t end \n"
+    File.open('./clock.rb','a') {|f| f.write(doc) }
+  when archive_schedule = "weekly"
+    doc = "\t every(1.week, 'For #{archive_schedule}', :at => '14:00') do \n \t \t ArchiveWorker.perform_async(\"#{@backup_path.gsub(/\/$/, '')}#{sub_folder[0]}\",\"#{@server_name}\",\"#{archive_schedule}\") \n  \t end \n"
+    File.open('./clock.rb','a') {|f| f.write(doc) }
+  end
+
+end
+end
+end
+end
+
+end
+
+
+def yml_process 
+ yml_files = " "
+ yml_files << `ls  ./config/*.yml |xargs`
+ if yml_files.empty? 
+   puts "Err list empty"	
+ else
+   doc = "# ClockWork scheduler generated at [#{Time.new}] \n require \"clockwork\" \n \n Dir[\"./lib/rsync.rb\"].each {|file| require file } \n Dir[\"./run.rb\"].each {|file| require file } \n \n module Clockwork \n handler do |job| \n \t puts \"Running \#{job}\" \n end  \n" 
+
+   File.open('./clock.rb','w+') {|f| f.write(doc) }
+
+   yml_files.split(" ").each do |yml_file|
+     a = YmlProcess.new(yml_file)
+     a.read_config
+     a.generate_rsync_job(yml_file)
+     a.generate_compress_schedule
+   end
+   File.open('./clock.rb','a') {|f| f.write("  end \n") }
  end
+end
